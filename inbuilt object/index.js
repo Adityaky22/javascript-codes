0109d8b3057c332d67104c094f0ac3ebc04ffbe7
Adityaky22 <110656201+Adityaky22@users.noreply.github.com>
2023-09-16 14:47:42 +0530

    task completed 15/9/2023

---
+++ inbuilt object/index.js
@@ -0,0 +1,38 @@
+//math
+//math.round()and there are many fuction or inbuit object you can access using mdn
+console.log('hello ji');
+
+
+//second 
+//string string are of two typwes in javascripts
+//peimitive type
+let lastname ='yadav';
+//to conbert in object
+lastname.charAt
+
+//object
+let firstname = new String('Aditya');
+
+let message = 'this is my first message';
+let words= message.split(' ');
+console.log(words);
+
+
+//array
+//creation 
+let numbers = [1,2,3,4];
+
+//insertion 
+//insert at front
+numbers.push(0);
+console.lof(numbers);
+
+//insertion at last 
+numbers.unshift(5);
+console.log(numbers);
+
+//inertyion at middel formate (index to insert , which eleemnt want to delete, number to insert)
+
+numbers.splice(3,0,4578);
+console.log(numbers);
+
